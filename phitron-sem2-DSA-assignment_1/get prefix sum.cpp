6e4789e87f7939476fcf92fda8d59e222d3aa995
mugdha-sarker81 <mugdhasarker81@gmail.com>
2026-06-27 20:03:12 +0600

    Update get prefix sum.cpp

--- phitron-sem2-DSA-assignment_1/get prefix sum.cpp
+++ phitron-sem2-DSA-assignment_1/get prefix sum.cpp
@@ -1,9 +1,14 @@
 /*link : https://www.hackerrank.com/contests/assignment-01-a-basic-data-structure-a-batch-09/challenges/get-prefix-sum/problem?isFullScreen=true
 question:
 i got am int array and have to print the prefix sum for this array in reverse order.
-answer:
-take an array as the same size as given array . and have to put the sum of index 0 to i , on the position i. 
+observation:
+take an array as the same size as given array . and have to put the sum of index 0 to i  on the position i. 
 and lastly print the sum array in reverse order. note: the first value of both array is same ;
+testcase analysis:
+5
+givn array : 1 2 3 4 5 
+prefix sum : 1 3 6 10 15
+output     : 15 10 6 3 1
 time complexity: O(N)
 */
 
